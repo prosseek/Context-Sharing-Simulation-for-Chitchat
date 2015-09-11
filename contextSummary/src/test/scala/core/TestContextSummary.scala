4054b0ebc400ba10a445b80a39359d9c91e3000d
smcho <sm.cho@mac.com>
2015-09-10 22:35:42 -0500

    test contextSummary OK

--- contextSummary/src/test/scala/core/TestContextSummary.scala
+++ contextSummary/src/test/scala/core/TestContextSummary.scala
@@ -1,5 +1,7 @@
 package core
 
+import java.io.File
+
 import grapevineType.BottomType.BottomType
 import org.scalatest.{BeforeAndAfter, FunSuite}
 
@@ -28,7 +30,49 @@ class TestContextSummary extends FunSuite with BeforeAndAfter {
     c = C(filePath)
   }
 
+  test("test map values") {
+    //    {
+    //      "latitude": [10, 10, 10, 10],
+    //      "message": ,
+    //      "time": [11, 11],
+    //      "date":[2014, 10, 1]
+    //    }
+    val m = c.getJsonMap()
+    assert(m.get("latitude").get == (10,10,10,10))
+    assert(m.get("message").get == "Hello, world")
+    assert(m.get("time").get == (11,11))
+    assert(m.get("date").get == (2014,10,1))
+  }
+
+  test("test Load") {
+    // c is already loaded with unittest.json
+    assert(c.getJsonSize() == 107)
+    val filePath = "contextSummary/src/test/scala/resource/simple.json"
+    c.load(filePath)
+    assert(c.getJsonSize() == 55)
+  }
+
+  test("test ToString") {
+    val result =
+      """|{
+         |  "date":[2014, 10, 1],
+         |  "latitude":[10, 10, 10, 10],
+         |  "message":"Hello, world",
+         |  "time":[11, 11]
+         |}
+         |""".stripMargin
+
+    assert(c.toString() == result)
+  }
+
   test("test save") {
-    c.save("contextSummary/src/test/scala/resource/unittest2.json")
+    val filePath = "contextSummary/src/test/scala/resource/unittest_test_result.json"
+    val fp = new File(filePath)
+    if (fp.exists()) {
+      fp.delete()
+    }
+    assert(new File(filePath).exists() == false)
+    c.save(filePath)
+    assert(new File(filePath).exists())
   }
 }
